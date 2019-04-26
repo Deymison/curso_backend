c4bd5145181e8cda70a241a3c37bdf8878e8988a
Deymison <deymison.oliveira@mobly.com.br>
2019-04-26 16:12:00 -0300

    adicionando arquivos novos

--- loja/produto_del.php
+++
@@ -1,8 +0,0 @@
-<?php include("cabecalho.php");
- include("conecta.php");
- include("banco_produto.php");
- $id=$_GET['id'];
- removeProduto($conexao,$id);
- header("location: produto_list.php?removido=true");
- die();
- ?>
